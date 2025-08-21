efd0bcb72a1f422a3f0adcec8013c6f1fbe7c3db
joecoh <joashelangovan@gmail.com>
2025-08-21 22:31:06 +0530

    Added React portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import Header from './component/Header';
+import About from './component/About';
+import Projects from './component/Projects';
+import Contact from './component/Contact';
 
 function App() {
   return (
